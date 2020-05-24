b273594436326c5d5a6e2fd1047a63bf21f8e1f3
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-05-24 14:58:10 -0300

    feat: header dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { useAuth } from '../../hooks/auth';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
+import logo from '../../assets/images/logo.svg';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+
+            <button type="button" onClick={signOut}>
+              <FiPower />
+            </button>
+          </Profile>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;

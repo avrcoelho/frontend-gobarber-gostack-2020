52b1ec7a9c20093114757da7f7dace5f625789f1
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-05-25 21:47:36 -0300

    feat: update avatar

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -22,6 +22,7 @@ interface AuthContextData {
   user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(uer: User): void;
 }
 
 // as AuthContext)
@@ -64,8 +65,22 @@ export const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
